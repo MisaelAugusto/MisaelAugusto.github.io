c5e95ce9e2f8bf19c8ae549376ce0fb5467d22dd
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-07-12 10:54:26 -0300

    Updates

---
+++ precache-manifest.22d04ed0093b8a97c090eb484a426f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e341f7c1869b364f9b0772b51218303d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fb823a0a927e135723",
+    "url": "/static/js/2.2df53013.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2df53013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c6509efa8904e9dff1",
+    "url": "/static/js/main.3e4e5b76.chunk.js"
+  },
+  {
+    "revision": "319e95d3e238018a02d0",
+    "url": "/static/js/runtime-main.0c1849c4.js"
+  }
+]);
