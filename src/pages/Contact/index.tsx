7e13f8a1d8df5555f4c183fba1ea8db6c23e5e23
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-07-17 14:55:27 -0300

    Add contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import {} from 'react-icons';
 import { Link } from 'react-router-dom';
 
-import { Header, ContactInfo } from './styles';
+import { Header, Option, Menu } from './styles';
+
+import Github from '../../assets/contacts/github.svg';
+import Email from '../../assets/contacts/email.svg';
+import Linkedin from '../../assets/contacts/linkedin.svg';
 
 const Contact: React.FC = () => (
   <>
@@ -13,7 +16,29 @@ const Contact: React.FC = () => (
         Augusto
       </Link>
     </Header>
-    <ContactInfo />
+    <Menu>
+      <Option>
+        <a href="https://github.com/MisaelAugusto" draggable={false}>
+          <img src={Github} alt="Github" draggable={false} />
+        </a>
+      </Option>
+      <Option>
+        <a
+          href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=misael.costa@ccc.ufcg.edu.br&tf=1"
+          draggable={false}
+        >
+          <img src={Email} alt="Email" draggable={false} />
+        </a>
+      </Option>
+      <Option>
+        <a
+          href="https://www.linkedin.com/in/misael-augusto-b04073192/"
+          draggable={false}
+        >
+          <img src={Linkedin} alt="Linkedin" draggable={false} />
+        </a>
+      </Option>
+    </Menu>
   </>
 );
 
--- src/pages/Contact/styles.ts
+++ src/pages/Contact/styles.ts
@@ -7,15 +7,21 @@ export const Header = styled.header`
   padding: 18px 80px;
 
   a {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
     color: black;
     font-size: 24px;
     text-decoration: none;
+
+    & + a {
+      margin-left: 12px;
+    }
   }
 
   #misael {
     padding-left: 8px;
     border-left: 4px solid #ffdd50;
-
     transition: transform 0.2s;
 
     &:hover {
@@ -24,4 +30,24 @@ export const Header = styled.header`
   }
 `;
 
-export const ContactInfo = styled.div``;
+export const Option = styled.div`
+  margin: 28px 0 0 0;
+  font-size: 32px;
+  margin-right: 60px;
+  position: relative;
+  transition: transform 0.2s;
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 80px;
+`;
